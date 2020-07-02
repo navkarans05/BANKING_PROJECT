b4c53f5aafbc081057cb303082d76f16e7832ee5
navkarans05 <navkarans1405@gmail.com>
2020-07-02 19:52:27 +0530

    Commit

--- pro_intern/settings.py
+++ pro_intern/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd5#nx6dk7$mxoz0cneae%zi%)j#rs=q(vtug32p+6z5^7%p7+_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG_VALUE"))=="True"
 
-ALLOWED_HOSTS = ['127.0.0.1' ,'https://dhillon-banking.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1' ,'https://dhillon-bankingapp.herokuapp.com']
 
 
 # Application definition
